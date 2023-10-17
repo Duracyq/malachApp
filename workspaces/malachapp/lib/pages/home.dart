ee52f1fa3ad91013b4d789de90358b161427981d
Fortland2018 <adamkowalskipl68@gmail.com>
2023-10-17 22:14:37 +0200

    wiktor usnal

---
+++ workspaces/malachapp/lib/pages/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:malachapp/themes/dark_mode.dart';
+import 'package:malachapp/themes/light_mode.dart';
+
+class TopBarFb2 extends StatelessWidget {
+  final String title;
+  final String upperTitle;
+  const TopBarFb2({required this.title, required this.upperTitle, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title,
+            style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 14,
+                fontWeight: FontWeight.normal)),
+        Text(upperTitle,
+            style: const TextStyle(
+                color: Colors.black, fontSize: 26, fontWeight: FontWeight.bold))
+      ],
+    );
+  }
+}
---
+++ workspaces/malachapp/lib/pages/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:malachapp/themes/dark_mode.dart';
+import 'package:malachapp/themes/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowMaterialGrid: false,
+      theme: lightMode,
+      darkTheme: darkMode,
+    );
+  }
+}
