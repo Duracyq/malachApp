d7954cbf9108bceee753d1f7a732d295a89228bb
Fortland2018 <adamkowalskipl68@gmail.com>
2023-12-06 22:55:53 +0100

    naprawianie

--- workspaces/malachapp/lib/pages/home.dart
+++ workspaces/malachapp/lib/pages/home.dart
@@ -9,7 +9,6 @@ import 'package:malachapp/themes/dark_mode.dart';
 import 'package:malachapp/themes/light_mode.dart';
 import 'dart:ui' as ui;
 
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -124,13 +123,6 @@ class HomeHome extends StatelessWidget {
                                 child: CachedNetworkImage(
                                   // Use CachedNetworkImage instead of Image.network
                                   imageUrl: snapshot.data![index],
-<<<<<<< HEAD
-                                  fit: BoxFit.cover,
-                                  placeholder: (context, url) =>
-                                      const CircularProgressIndicator(),
-                                  errorWidget: (context, url, error) =>
-                                      const Icon(Icons.error),
-=======
                                   // imageBuilder: (context, imageProvider) => Container(
                                   //   decoration: BoxDecoration(
                                   //     image: DecorationImage(
@@ -139,9 +131,10 @@ class HomeHome extends StatelessWidget {
                                   //     )
                                   //   ),
                                   // ),
-                                  placeholder: (context, url) => const CircularProgressIndicator(),
-                                  errorWidget: (context, url, error) => const Icon(Icons.error),
->>>>>>> b9f3882c097825ebf4beeb2663d9e34aa0467c8d
+                                  placeholder: (context, url) =>
+                                      const CircularProgressIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error),
                                 ),
                               );
                             },
@@ -160,7 +153,6 @@ class HomeHome extends StatelessWidget {
                   // Text from Firestore Cloud DB
                   StreamBuilder(
                     stream: firebaseFirestore.collection('test').snapshots(),
-<<<<<<< HEAD
                     builder: (BuildContext context,
                         AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
                             snapshot) {
@@ -177,20 +169,6 @@ class HomeHome extends StatelessWidget {
                           children: snapshot.data!.docs.map(
                               (QueryDocumentSnapshot<Map<String, dynamic>>
                                   document) {
-=======
-                    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-                      if (snapshot.hasError) {
-                        return Text('Error: ${snapshot.error}');
-                      }
-          
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const CircularProgressIndicator();
-                      }
-          
-                      return Expanded(
-                        child: ListView(
-                          children: snapshot.data!.docs.map((QueryDocumentSnapshot<Map<String, dynamic>> document) {
->>>>>>> b9f3882c097825ebf4beeb2663d9e34aa0467c8d
                             Map<String, dynamic> data = document.data();
                             return ListTile(
                               title: Text(data['test']),
