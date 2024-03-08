d93cdc017d2168f27d7e36d89d8a83067b0fca94
Fortland2018(Wiktor_Zabka) <adamkowalskipl68@gmail.com>
2024-03-08 23:23:42 +0100

    design w add_member

--- workspaces/malachapp/lib/pages/add_group_page.dart
+++ workspaces/malachapp/lib/pages/add_group_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:malachapp/auth/auth_service.dart';
+import 'package:malachapp/components/my_button.dart';
 import 'package:malachapp/components/text_field.dart';
 
 class AddGroupPage extends StatefulWidget {
@@ -48,12 +49,14 @@ class _AddGroupPageState extends State<AddGroupPage> {
                       content: Text('Group added successfully!'),
                     ),
                   );
-                  Navigator.pop(context); // Navigate back to the previous screen
+                  Navigator.pop(
+                      context); // Navigate back to the previous screen
                 } else {
                   // Show a message if the user is not an admin
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text('You do not have permission to add groups.'),
+                      content:
+                          Text('You do not have permission to add groups.'),
                     ),
                   );
                 }
@@ -66,8 +69,6 @@ class _AddGroupPageState extends State<AddGroupPage> {
     );
   }
 }
-
-
 
 class AddMemberPage extends StatefulWidget {
   const AddMemberPage({super.key});
@@ -86,25 +87,29 @@ class _AddMemberPageState extends State<AddMemberPage> {
       appBar: AppBar(
         title: const Text('Add Member'),
       ),
-      body: Center(
-        child: Column(
-          children:[
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Enter member email',
-              ),
-              controller: memberEmailController,
-            ),
-            IconButton(
-              onPressed: () {
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+            child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyTextField(
+                hintText: 'Enter member email',
+                controller: memberEmailController),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MyButton(
+              text: "Dodaj",
+              onTap: () {
                 _db.collection('groups').doc('groupID').update({
-                  'members': FieldValue.arrayUnion([memberEmailController.text]),
+                  'members':
+                      FieldValue.arrayUnion([memberEmailController.text]),
                 });
               },
-              icon: const Icon(Icons.add),
             ),
-          ] 
-        )
+          )
+        ])),
       ),
     );
   }
