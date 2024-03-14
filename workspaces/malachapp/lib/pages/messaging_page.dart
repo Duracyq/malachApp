72aacc375c8e30a5fb8016cdca8cd8de3226e417
Duracyq <szymon.kojek@wp.pl>
2024-03-14 01:42:34 +0100

    custom domain names in adding members

--- workspaces/malachapp/lib/pages/messaging_page.dart
+++ workspaces/malachapp/lib/pages/messaging_page.dart
@@ -60,7 +60,7 @@ class _MessagingPageState extends State<MessagingPage> {
                         PageRouteBuilder(
                           pageBuilder:
                               (context, animation, secondaryAnimation) =>
-                                  const AddMemberPage(),
+                                  AddMemberPage(groupID: widget.groupId),
                           transitionsBuilder:
                               (context, animation, secondaryAnimation, child) {
                             var begin = const Offset(1.0, 0.0);
