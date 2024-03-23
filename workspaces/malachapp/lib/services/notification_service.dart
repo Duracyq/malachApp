23c8585e6f735af0295aa2a44dc6f6bf8d23caf7
Duracyq <szymon.kojek@wp.pl>
2024-03-23 20:04:23 +0100

    fixes

--- workspaces/malachapp/lib/services/notification_service.dart
+++ workspaces/malachapp/lib/services/notification_service.dart
@@ -17,7 +17,7 @@ class NotificationService {
   Future<String> getOAuth2TokenFromFirebaseStorage() async {
     try {
       // Path to the secret in Firebase Storage
-      const String secretPath = 'secret/malachapp-firebase-adminsdk-9a5ko-d405b11463.json';
+      const String secretPath = 'secret/malachapp-firebase-adminsdk-9a5ko-7e05d828f3.json';
 
       // Retrieve the secret file's content from Firebase Storage
       final ref = FirebaseStorage.instance.ref().child(secretPath);
