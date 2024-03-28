830fc6aa364223a189185e82a2f9981c8aab7940
Fortland2018(Wiktor_Zabka) <adamkowalskipl68@gmail.com>
2024-03-28 17:06:19 +0100

    60% design wydarzen

--- workspaces/malachapp/lib/pages/Events/event_design.dart
+++ workspaces/malachapp/lib/pages/Events/event_design.dart
@@ -1,13 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:ffi';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:malachapp/components/MyText.dart';
-import 'package:malachapp/pages/add_group_page.dart';
-import 'package:malachapp/services/nickname_fetcher.dart';
 
 class EventList extends StatelessWidget {
   const EventList({super.key});
@@ -15,48 +7,160 @@ class EventList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Events'),
-        ),
-        body: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                childAspectRatio: 9 / 16),
-            itemBuilder: (context, int i) {
-              return Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: const Offset(0, 3))
-                      ]),
-                  child: Column(children: [
-                    Container(
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                      image: AssetImage('assets/zd2.jpg'),
-                    ))),
-                    Row(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: Colors.blue,
-                          ),
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 4, horizontal: 8),
-                            child: Text("COS"),
-                          ),
-                        )
-                      ],
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Events"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.cart)),
+          IconButton(
+              onPressed: () {
+                // context.read<SignInBloc>().add(SignOutRequired());
+                print("Sign out button pressed");
+              },
+              icon: const Icon(CupertinoIcons.arrow_right_to_line)),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // number of columns
+              crossAxisSpacing: 10, // spacing between the columns
+              mainAxisSpacing: 10, // spacing between the rows
+              childAspectRatio: 0.6),
+          itemCount: 10, // replace with the number of events
+
+          itemBuilder: (context, int i) {
+            return Material(
+              elevation: 3,
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: InkWell(
+                borderRadius: BorderRadius.circular(20),
+                onTap: () {
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute<void>(
+                  //     builder: (BuildContext context) =>
+                  //         DetailsScreen(state.events[i]),
+                  //   ),
+                  // );
+                  print("Event $i tapped");
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                      child: Image.network(
+                        'https://fastly.picsum.photos/id/90/3000/1992.jpg?hmac=v_xO0GFiGn3zpcKzWIsZ3WoSoxJuAEXukrYJUdo2S6g',
+                        fit: BoxFit
+                            .cover, // this is to make sure the image covers the container
+                      ),
                     ),
-                  ]));
-            }));
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(30)),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 4, horizontal: 8),
+                              child: Text(
+                                "Category", // replace with the event category
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 10),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.green.withOpacity(0.2),
+                                borderRadius: BorderRadius.circular(30)),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 4, horizontal: 8),
+                              child: Text(
+                                "Date", // replace with the event date
+                                style: TextStyle(
+                                    color: Colors.green,
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 10),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Text(
+                        "Event name", // replace with the event name
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: Text(
+                        "Event description", // replace with the event description
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  "\$Price", // replace with the event price
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  "\$Original price", // replace with the event original price
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.grey.shade500,
+                                      fontWeight: FontWeight.w700,
+                                      decoration: TextDecoration.lineThrough),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(CupertinoIcons.add_circled_solid))
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
