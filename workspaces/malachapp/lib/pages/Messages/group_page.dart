56e823c022282ce2143f207d824bf3e2072818c9
Szymon Kajak <92056669+Duracyq@users.noreply.github.com>
2024-04-15 21:55:22 +0200

    fixes (#43)

--- workspaces/malachapp/lib/pages/Messages/group_page.dart
+++ workspaces/malachapp/lib/pages/Messages/group_page.dart
@@ -1,19 +1,16 @@
+import 'package:async/async.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 import 'package:malachapp/auth/auth_service.dart';
-import 'package:malachapp/components/MyText1.dart';
+import 'package:malachapp/components/MyText2.dart';
 import 'package:malachapp/components/reloadable_widget.dart';
 import 'package:malachapp/pages/Messages/messaging_page.dart';
 import 'package:malachapp/pages/add_group_page.dart';
-import 'package:malachapp/pages/notification_subs_page.dart';
-import 'package:malachapp/services/group_service.dart';
-import 'package:intl/intl.dart';
-import 'package:malachapp/services/nickname_fetcher.dart';
-import 'package:malachapp/services/notification_service.dart';
-import 'package:malachapp/services/subscribe_to_noti.dart';
 import 'package:provider/provider.dart';
+import 'package:malachapp/themes/dark_mode.dart';
+import 'package:malachapp/themes/theme_provider.dart';
 
 class GroupPage extends StatefulWidget {
   const GroupPage({super.key});
@@ -26,6 +23,7 @@ class _GroupPageState extends State<GroupPage> {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
   final AuthService _authService = AuthService();
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
   Future<String> _groupIDGetter(String groupTitle) async {
     try {
       QuerySnapshot querySnapshot = await _db
@@ -35,8 +33,7 @@ class _GroupPageState extends State<GroupPage> {
           .get();
 
       if (querySnapshot.docs.isNotEmpty) {
-        String groupId = querySnapshot.docs.first.id;
-        return groupId;
+        return querySnapshot.docs.first.id;
       }
     } catch (e) {
       print(e);
@@ -46,144 +43,167 @@ class _GroupPageState extends State<GroupPage> {
 
   Future<bool> isAdminAsync() async {
     User? user = _auth.currentUser;
-    if (user != null) {
-      return await _authService.isAdmin();
+    return user != null && await _authService.isAdmin();
+  }
+
+  Stream<List<Map<String, dynamic>>> getCombinedStream() async* {
+    var groupStream = _db.collection('groups').snapshots().map((snapshot) =>
+      snapshot.docs.map((doc) => {...doc.data(), 'id': doc.id, 'collection': 'groups'}).toList());
+
+    bool isAdmin = await isAdminAsync();
+
+    Stream<List<Map<String, dynamic>>> groupForClassStream;
+    if (isAdmin) {
+      groupForClassStream = _db.collection('groupsForClass').snapshots().map((snapshot) =>
+        snapshot.docs.map((doc) => {...doc.data(), 'id': doc.id, 'collection': 'groupsForClass'}).toList());
+    } else {
+      var userDoc = await _db.collection('users').doc(_auth.currentUser!.uid).get();
+      var userClass = userDoc.data()?['class'];
+      var userYear = userDoc.data()?['year'];
+      groupForClassStream = _db.collection('groupsForClass').snapshots().map((snapshot) =>
+        snapshot.docs.where((doc) => doc.data()['class'] == userClass && doc.data()['year'] == userYear).map((doc) => {...doc.data(), 'id': doc.id, 'collection': 'groupsForClass'}).toList());
     }
-    return false;
+
+    var combinedStream = StreamZip([groupStream, groupForClassStream])
+        .map((List<List<Map<String, dynamic>>> combinedData) => combinedData.expand((x) => x).toList());
+
+    await for (var combinedList in combinedStream) {
+      for (var group in combinedList) {
+        bool isMember = await this.isMember(group['id']);
+        group['isMember'] = isMember;
+      }
+      yield combinedList;
+    }
+  }
+
+  Future<bool> isMember(String groupId) async {
+    try {
+      var currentUserEmail = _auth.currentUser?.email;
+      if (currentUserEmail == null) return false;
+
+      DocumentSnapshot groupDoc = await _db.collection('groups').doc(groupId).get();
+      if (!groupDoc.exists) {
+        groupDoc = await _db.collection('groupsForClass').doc(groupId).get();
+        if (!groupDoc.exists) return false;
+      }
+
+      Map<String, dynamic>? groupData = groupDoc.data() as Map<String, dynamic>?;
+      List<dynamic> members = groupData?['members'] as List<dynamic>? ?? [];
+      return members.contains(currentUserEmail);
+    } catch (e) {
+      print("Error checking membership: $e");
+      return false;
+    }
   }
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-        future: isAdminAsync(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+Widget build(BuildContext context) {
+  return FutureBuilder<bool>(
+    future: isAdminAsync(),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const Center(child: CircularProgressIndicator());
+      }
 
-          bool isAdmin = snapshot.data ?? false;
+      bool isAdmin = snapshot.data ?? false;
 
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Group Page'),
-            ),
-            body: Container(
-              padding: const EdgeInsets.fromLTRB(
-                  12.0, 20.0, 12.0, 0.0), // Dodaj odstęp od góry
-              child: ReloadableWidget(
-                onRefresh: () async {
-                  setState(() {
-                    // Refresh logic here
-                  });
+      return Scaffold(
+        appBar: AppBar(title: const Text('Group Page')),
+        body: Container(
+          padding: const EdgeInsets.fromLTRB(12.0, 20.0, 12.0, 0.0),
+          child: ReloadableWidget(
+            onRefresh: () async {
+              setState(() {});
+            },
+            child: SingleChildScrollView(
+              child: StreamBuilder<List<Map<String, dynamic>>>(
+                stream: getCombinedStream(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+                  if (!snapshot.hasData || snapshot.data == null) {
+                    return const Center(child: Text('No groups available...'));
+                  }
+
+                  List<Widget> children = [];
+                  for (int i = 0; i < snapshot.data!.length; i++) {
+                    final data = snapshot.data![i];
+                    String prefix = data['collection'] == 'groupsForClass' ? '' : '';
+                    List<dynamic> members = data['members'] as List<dynamic>? ?? [];
+                    bool isUserMember(String userEmail) {
+                      return members.isNotEmpty && members.contains(userEmail);
+                    }
+
+                    if (data['collection'] == 'groupsForClass' || isUserMember(_auth.currentUser?.email ?? '') || isAdmin) {
+                      children.add(
+                        Card(
+                          color: !(data['collection'] == 'groupsForClass' || isUserMember(_auth.currentUser?.email ?? ''))
+                            ? (Provider.of<ThemeProvider>(context, listen: false).themeData == darkMode ? Colors.black54 : Colors.grey)
+                            : (Provider.of<ThemeProvider>(context, listen: false).themeData == darkMode ? Colors.grey[700] : Colors.white),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ListTile(
+                              title: Text('$prefix${data['groupTitle']}'),
+                              onTap: () async {
+                                if (data['collection'] == 'groupsForClass') {
+                                  String groupId = data['id'];
+                                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => MessagingPage(groupId: groupId, groupTitle: data['groupTitle'], isGFC: true)));
+                                } else {
+                                  if (isUserMember(_auth.currentUser?.email ?? '') || isAdmin) {
+                                    String groupId = await _groupIDGetter(data['groupTitle']);
+                                    Navigator.of(context).push(MaterialPageRoute(builder: (context) => MessagingPage(groupId: groupId, groupTitle: data['groupTitle'], isGFC: false)));
+                                  } else {
+                                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('You do not have access to view this group!')));
+                                  }
+                                }
+                              },
+                            ),
+                          ),
+                        )
+                      );
+                    }
+
+                    if (i < snapshot.data!.length - 1) {
+                      if (data['collection'] != snapshot.data![i + 1]['collection']) {
+                        children.add(Wrap(
+                          direction: Axis.horizontal,
+                          children: [
+                            const MyText2(text: 'Class Chats', rozmiar: 21),
+                            Divider(color: Provider.of<ThemeProvider>(context).themeData == darkMode ? Colors.white : Colors.black, thickness: 2),
+                          ]
+                        ));
+                      }
+                    }
+                  }
+
+                  return ListView(
+                    primary: false,  // Make sure the ListView does not scroll
+                    shrinkWrap: true,  // Let ListView size itself according to the incoming content
+                    children: [...children, const SizedBox(height: 20)],
+                  );
                 },
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: _db.collection('groups').snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    if (snapshot.hasError) {
-                      return Center(
-                        child: Text('Error: ${snapshot.error}'),
-                      );
-                    }
-                    final groups = snapshot.data!.docs.map((doc) {
-                      final data = doc.data() as Map<String, dynamic>;
-                      return {
-                        ...data,
-                        'groupTitle': data['groupTitle'] ?? 'No title'
-                      };
-                    }).toList();
-
-                    if (groups.isEmpty) {
-                      return const Center(
-                          child: Text('No groups available...'));
-                    }
-                    return ListView.builder(
-                      itemCount: groups.length,
-                      itemBuilder: (context, index) {
-                        final doc = groups[index];
-
-                        return Container(
-                          padding:
-                              const EdgeInsets.all(10), // Zwiększ padding do 10
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 5,
-                              horizontal: 10), // Dodaj margines poziomy
-                          decoration: BoxDecoration(
-                            // color: Colors.white,
-                            borderRadius: BorderRadius.circular(
-                                15), // Zwiększ promień zaokrąglenia
-                            boxShadow: [
-                              // Dodaj cień
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 5,
-                                blurRadius: 7,
-                                offset: const Offset(0, 3),
-                              ),
-                            ],
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              ListTile(
-                                title: MyText1(
-                                  text: doc['groupTitle'],
-                                  rozmiar: 18,
-                                ),
-                                // title: Text(
-                                //   ,
-                                //   style: const TextStyle(
-                                //     fontWeight: FontWeight.bold,
-                                //     fontSize: 18,
-                                //   ),
-                                // ),
-                                onTap: () async {
-                                  String groupId =
-                                      await _groupIDGetter(doc['groupTitle']);
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                      builder: (context) => MessagingPage(
-                                            groupId: groupId,
-                                            groupTitle: doc['groupTitle'],
-                                          )));
-                                },
-                              ),
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
               ),
             ),
-            floatingActionButton: isAdmin
-                ? FloatingActionButton(
-                    onPressed: () => Navigator.of(context).push(
-                      MaterialPageRoute(
-                          builder: ((context) => const AddGroupPage())),
-                    ),
-                  )
-                : null,
-          );
-        });
-  }
+          ),
+        ),
+        floatingActionButton: isAdmin ? FloatingActionButton(onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: ((context) => const AddGroupPage()))), child: const Icon(Icons.add)) : null,
+      );
+    },
+  );
+}
+
 }
 
 String formatMessageTime(Timestamp timestamp) {
   DateTime messageTime = timestamp.toDate();
   DateTime now = DateTime.now();
-
   if (now.difference(messageTime).inHours < 24) {
-    // If the message was sent within the last 24 hours, display only the time
-    return DateFormat('HH:mm').format(messageTime);
+    return DateFormat('HH:mm').format(messageTime);  // Display only the time for recent messages
   } else {
-    // If the message was sent more than 24 hours ago, display the full date and time
-    return DateFormat('yyyy-MM-dd HH:mm').format(messageTime);
+    return DateFormat('yyyy-MM-dd HH:mm').format(messageTime);  // Display the full date for older messages
   }
 }
--- workspaces/malachapp/lib/pages/Messages/messaging_page.dart
+++ workspaces/malachapp/lib/pages/Messages/messaging_page.dart
@@ -1,11 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:malachapp/auth/auth_service.dart';
-import 'package:malachapp/components/MyText1.dart';
-import 'package:malachapp/components/reloadable_widget.dart';
-import 'package:malachapp/pages/Messages/group_page.dart';
 import 'package:malachapp/pages/add_group_page.dart';
 import 'package:malachapp/pages/notification_subs_page.dart';
 import 'package:malachapp/services/group_service.dart';
@@ -18,8 +14,9 @@ import 'package:provider/provider.dart';
 class MessagingPage extends StatefulWidget {
   final String groupId;
   final String? groupTitle;
+  final bool isGFC;
 
-  const MessagingPage({super.key, required this.groupId, this.groupTitle});
+  const MessagingPage({super.key, required this.groupId, this.groupTitle, required this.isGFC});
 
   @override
   _MessagingPageState createState() => _MessagingPageState();
@@ -32,13 +29,16 @@ class _MessagingPageState extends State<MessagingPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final AuthService _authService = AuthService();
   late SubscribeNotifications _subscribeNotifications;
+  late bool isGFC;
   late UserNotificationPreferences _notificationPreferences;
 
   @override
   void initState() {
     super.initState();
+    isGFC = widget.isGFC;
     _subscribeNotifications = SubscribeNotifications();
     _notificationPreferences = UserNotificationPreferences();
+    isMemberSubscribed();
     _checkSubscription();
   }
 
@@ -88,17 +88,15 @@ class _MessagingPageState extends State<MessagingPage> {
                 child: Row(
                   children: [
                     Visibility(
-                      visible: isAdmin,
+                      visible: isAdmin && !isGFC,
                       child: IconButton(
                         icon: const Icon(Icons.person_add),
                         onPressed: () {
                           Navigator.of(context).push(
                             PageRouteBuilder(
-                              pageBuilder:
-                                  (context, animation, secondaryAnimation) =>
-                                      AddMemberPage(groupID: widget.groupId),
-                              transitionsBuilder: (context, animation,
-                                  secondaryAnimation, child) {
+                              pageBuilder: (context, animation, secondaryAnimation) =>
+                                  AddMemberPage(groupID: widget.groupId),
+                              transitionsBuilder: (context, animation, secondaryAnimation, child) {
                                 var begin = const Offset(1.0, 0.0);
                                 var end = Offset.zero;
                                 var curve = Curves.ease;
@@ -144,7 +142,6 @@ class _MessagingPageState extends State<MessagingPage> {
                         );
                       },
                     ),
-                    // IconButton(onPressed: () => SubscribeNotifications().unsubscribeFromGroupTopic(widget.groupId), icon: Icon(Icons.cancel)),
                   ],
                 ),
               )
@@ -154,7 +151,6 @@ class _MessagingPageState extends State<MessagingPage> {
             children: <Widget>[
               Expanded(
                 child: StreamBuilder(
-                  // Use StreamBuilder to listen for real-time updates
                   stream: _db
                       .collection('groups')
                       .doc(widget.groupId)
@@ -170,19 +166,15 @@ class _MessagingPageState extends State<MessagingPage> {
                         return Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: Row(
-                            crossAxisAlignment: CrossAxisAlignment
-                                .start, // aligns the children at the start
+                            crossAxisAlignment: CrossAxisAlignment.start, // aligns the children at the start
                             children: [
                               const CircleAvatar(
                                 child: Icon(Icons.person),
                               ),
-                              const SizedBox(
-                                  width:
-                                      10), // add some space between the avatar and the text
+                              const SizedBox(width: 10), // add some space between the avatar and the text
                               Expanded(
                                 child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment
-                                      .start, // aligns the text to the start
+                                  crossAxisAlignment: CrossAxisAlignment.start, // aligns the text to the start
                                   children: [
                                     Row(
                                       children: [
@@ -192,8 +184,7 @@ class _MessagingPageState extends State<MessagingPage> {
                                               : message['sender'],
                                           style: const TextStyle(
                                               fontSize: 12,
-                                              color: Colors
-                                                  .grey), // smaller, grey text
+                                              color: Colors.grey), // smaller, grey text
                                         ),
                                         const Spacer(),
                                         Text(
@@ -253,19 +244,14 @@ class _MessagingPageState extends State<MessagingPage> {
                       onPressed: () async {
                         if (_messageController.text.isNotEmpty) {
                           await _groupService.sendMessage(
-                              widget.groupId,
-                              _messageController.text,
-                              _auth.currentUser!.email!);
+                            widget.groupId,
+                            _messageController.text,
+                            _auth.currentUser!.email!
+                          );
                           String nicknameTemp = _messageController.text;
                           _messageController.clear();
-                          String nickname = await NicknameFetcher()
-                              .fetchNickname(_auth.currentUser!.uid)
-                              .first;
-                          await NotificationService()
-                              .sendPersonalisedFCMMessage(
-                                  '$nickname: $nicknameTemp',
-                                  widget.groupId,
-                                  widget.groupTitle ?? 'Group Message');
+                          String nickname = await NicknameFetcher().fetchNickname(_auth.currentUser!.uid).first;
+                          await NotificationService().sendPersonalisedFCMMessage('$nickname: $nicknameTemp', widget.groupId, widget.groupTitle ?? 'Group Message');
                         }
                       },
                     ),
@@ -278,5 +264,18 @@ class _MessagingPageState extends State<MessagingPage> {
         );
       },
     );
+  }
+}
+
+String formatMessageTime(Timestamp timestamp) {
+  DateTime messageTime = timestamp.toDate();
+  DateTime now = DateTime.now();
+
+  if (now.difference(messageTime).inHours < 24) {
+    // If the message was sent within the last 24 hours, display only the time
+    return DateFormat('HH:mm').format(messageTime);
+  } else {
+    // If the message was sent more than 24 hours ago, display the full date and time
+    return DateFormat('yyyy-MM-dd HH:mm').format(messageTime);
   }
 }
